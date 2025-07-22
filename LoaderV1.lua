7145f78037926f858ba4915d20ac4ab5f8e46f97
NoLag-id <sfpher6@gmail.com>
2025-07-22 20:45:05 +0800

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,2 +1,2 @@
-loadstring(game:HttpGet("https://pastefy.app/HyteL83s/raw"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/mXDd8nWdZnVtfdvz/raw", true))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/azbalalaff/loading-screen/refs/heads/main/loader.lua"))()
