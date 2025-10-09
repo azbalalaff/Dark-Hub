560e7dd01a2a3027057fdd1ae079b23d6eb283ec
NoLag-id <sfpher6@gmail.com>
2025-10-09 20:45:36 +0800

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,3 +1,2 @@
-ID="ba003d4d-826a-4d61-b122-d0a0f793eec7";
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://pastefy.app/I7xJm2Js/raw"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/azbalalaff/loading-screen/refs/heads/main/loader.lua"))()
