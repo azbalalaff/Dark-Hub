e6d6486d6a51af141935017df91b9defd95a6205
NoLag-id <sfpher6@gmail.com>
2025-10-21 01:46:10 +0800

    Update LoaderV1.lua

--- LoaderV1.lua
+++ LoaderV1.lua
@@ -1,2 +1,2 @@
-ID="ba003d4d-826a-4d61-b122-d0a0f793eec7";
-loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/lrHLZX3YZHbAvM1m/raw"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/azbalalaff/loading-screen/refs/heads/main/loader.lua"))()
